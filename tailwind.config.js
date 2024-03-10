525e02a48e81c51a3e40479322d5d734b2443b9f
Ivan <skrillexgamesdef@gmail.com>
2024-03-10 21:45:21 +0100

    no funca las fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lato: ["Lato-Regular", "Lato-Thin", "Lato-Bold", "Lato-Black", "sans-serif"],
+        prata: ["Prata-Regular", "sans-serif"], 
+        ttCommons: ["TTCommonsPro-Regular", "sans-serif"], 
+      },
+    },
   },
   plugins: [],
-}
-
+};
