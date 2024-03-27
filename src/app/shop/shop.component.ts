a0fc516b56f2ddc116c4011b3e85119cab97b9a8
Ivan <skrillexgamesdef@gmail.com>
2024-03-27 15:46:50 +0100

    Shop bien maquetado

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { EuropeNumberPipe } from "../europe-number.pipe";
 
 @Component({
-  selector: 'app-shop',
-  standalone: true,
-  imports: [],
-  templateUrl: './shop.component.html',
-  styleUrl: './shop.component.css'
+    selector: 'app-shop',
+    standalone: true,
+    templateUrl: './shop.component.html',
+    styleUrl: './shop.component.css',
+    imports: [EuropeNumberPipe]
 })
 export class ShopComponent {
 
