8f516ae373f22a6c609b8b66c6f268fd55af0864
Ivan <skrillexgamesdef@gmail.com>
2024-03-27 19:03:01 +0100

    Sell your watch hecho

--- src/app/sell-your-watch/sell-your-watch.component.ts
+++ src/app/sell-your-watch/sell-your-watch.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sell-your-watch',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './sell-your-watch.component.html',
   styleUrl: './sell-your-watch.component.css'
 })
