09df9a11eed9482ae80a37a8f49b86cbcef3f9ca
Ivan <skrillexgamesdef@gmail.com>
2024-03-28 18:24:30 +0100

    Add-watch & add-news hecho

--- src/app/add-watch/add-watch.component.ts
+++ src/app/add-watch/add-watch.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-watch',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './add-watch.component.html',
   styleUrl: './add-watch.component.css'
 })
