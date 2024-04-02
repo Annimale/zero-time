02e0e9a7fdb59d2978ee72663fac339a9dcec023
Ivan <skrillexgamesdef@gmail.com>
2024-04-02 20:08:57 +0200

    EMepzamos backend

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const User = sequelize.define("User", {
+  // Define los atributos del modelo
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true, // Esto hará que se genere automáticamente el ID
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  role: {
+    type: DataTypes.ENUM('admin', 'user'),
+    defaultValue: 'user' // Valor por defecto es 'user'
+  },
+  // Continúa definiendo el resto de los campos...
+});
+
+module.exports = User;
