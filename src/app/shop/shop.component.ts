1a8aab7c386ed8648a1ba44dba6808a35865cc74
Ivan <skrillexgamesdef@gmail.com>
2024-04-23 12:27:58 +0200

    Falta por arreglar la shop que recorrar los relojes que tenemos en mi DB

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { EuropeNumberPipe } from "../europe-number.pipe";
-
+import { BrandService } from '../brand.service';
+import { CommonModule } from '@angular/common';
 @Component({
     selector: 'app-shop',
     standalone: true,
     templateUrl: './shop.component.html',
     styleUrl: './shop.component.css',
-    imports: [EuropeNumberPipe]
+    imports: [EuropeNumberPipe,CommonModule]
 })
 export class ShopComponent {
-
+    brands: any[] = [];
+    constructor(
+        private brandService: BrandService,
+    ) { }
+    ngOnInit(): void {
+        this.brandService.getAllBrands().subscribe({
+            next: (data) => {
+                console.log("Brands loaded:", data);
+                this.brands = data;
+            },
+            error: (error) => {
+                console.error("Failed to load brands. Response:", error);
+                console.error("Error details:", error.error.text || error.error);  // Attempting to capture non-JSON error message
+            }
+        });
+    }
 }
