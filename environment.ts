2463a2a81c3347325ba95ef2721f63a03d61a2d9
Ivan <skrillexgamesdef@gmail.com>
2024-05-27 18:41:03 +0200

    yes

--- environment.ts
+++ environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   stripePublishableKey:
     'pk_test_51PH2D9AhfPl4PAkCVw3yXgPsF2XJQ60M5ybkGYQWrSDlhNEBRDmkBwEfqhI7WSiQmFpkacsEvDGl7arA4ifvuCFa00acax7RjO', 
-    apiUrl:'https://zero-time-98594ed424a4.herokuapp.com/'
+    
 };
