86043e32448b13f86e379e2b79fae8c12b1d98cc
Ivan <skrillexgamesdef@gmail.com>
2024-05-27 15:31:02 +0200

    try8

--- config/database.js
+++ config/database.js
@@ -1,19 +1,9 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-// Verificar si estamos en un entorno de producción (Heroku)
-if (process.env.NODE_ENV === 'production') {
-  // Usar la URL de conexión de la variable de entorno DATABASE_URL
-  const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'mysql',
-    logging: console.log,
-  });
-} else {
-  // Usar la configuración de desarrollo o prueba
-  const sequelize = new Sequelize('zero-time', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: console.log,
-  });
-}
+const sequelize = new Sequelize("zero-time", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  logging: console.log,
+});
 
 module.exports = sequelize;
